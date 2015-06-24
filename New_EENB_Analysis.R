9fbf27faa78ca188a5b46ae9a70521089eff0b8d
Talleyman <stephen522@earthlink.net>
2015-06-24 14:51:31 -0400

    Replaced with improved file

--- New_EENB_Analysis.R
+++ New_EENB_Analysis.R
@@ -2,6 +2,7 @@
 #Data set up
 setwd("~/GitHub/Metagenomic-Analysis-Part-2")
 source("TwoStage_Package_Code.R")
+#Grab all cleaned data
 path<-as.list(dir(pattern=".*cleaned.*"))
 my.data<-lapply(path, function(x) read.csv(x, header=T))
 #Remove relevant rows, row names, and the city column so the phenotype file will run properly
@@ -13,8 +14,13 @@ removeExtras<-function(data){
   newdata<-data[,-c(2:3)]
   return(newdata)
 }
+#NOTE: These filenames are used in this order 
+#since this is their order in the original folder (sorted alphabetically)
 filenames<-list("ECID", "GOFuncID","GOProcID","PFamID","Phyllo_Genus","TIGRFamID")
 my.new.data<-lapply(my.data, removeExtras)
+#Dump extraneous variables for saving a bit of memory
+remove(my.data)
+remove(path)
 for (i in 1:length(my.new.data)){
   try(TwoStage_Package(my.new.data[[i]], factors, paste(filenames[[i]],"sigtest","csv", sep="."), 1))
   try(TwoStage_Package(my.new.data[[i]], factors, paste(filenames[[i]],"sigtest-method2","csv",sep="."), 2))
