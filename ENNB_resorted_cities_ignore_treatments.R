0ff9050a75a4e08ed1b7add1d92736af4f23fdd2
Talleyman <stephen522@earthlink.net>
2015-08-19 17:35:51 -0400

    Added separation by cities

---
+++ ENNB_resorted_cities_ignore_treatments.R
@@ -0,0 +1,34 @@
+#Resort the metagenomic data, this time by city, ignoring treatments
+#First, get the data set up
+setwd("~/GitHub/Metagenomic-Analysis-Part-2")
+source("TwoStage_Package_Code.R")
+#Grab all cleaned data
+path<-as.list(dir(pattern=".*cleaned.*"))
+my.data<-lapply(path, function(x) read.csv(x, header=T))
+factors<-read.csv("phyllo_factors.csv",header=T)
+rownames(factors)<-factors$treatment<-NULL
+
+for (i in 1:length(my.data)){
+  my.data[[i]][,19]<-NA
+  colnames(my.data[[i]])[19]<-"PHYLLO30"
+  my.data[[i]]$PHYLLO30<-round((my.data[[i]]$PHYLLO28+my.data[[i]]$PHYLLO29)/2)
+}
+
+#Impute the new PHYLLO30 sample into the phenotype file
+factors[18,]<-NA
+factors[,1]<-sapply(factors[,1],as.character)
+factors[18,]<-c("PHYLLO30", "HF")
+factors[,1:2]<-sapply(factors[,1:2],as.factor)
+
+#Resort the samples according to which samples come from which city for the analysis
+for (i in 1:length(my.data)){
+  my.data[[i]]<-my.data[[i]][,c(1,2,15,16,17:19,3,8,12,9,13,14,4,6,10,5,7,11)]
+}
+
+#NOTE: These filenames are used in this order 
+#since this is their order in the original folder (sorted alphabetically)
+filenames<-list("ECID", "GOFuncID","GOProcID","PFamID","Phyllo_Genus","TIGRFamID")
+for (i in 1:length(my.data)){
+  try(TwoStage_Package(my.data[[i]], factors, paste(filenames[[i]],"citysort","sigtest","csv", sep="."), 1))
+  try(TwoStage_Package(my.data[[i]], factors, paste(filenames[[i]],"citysort","sigtest-method2","csv",sep="."), 2))
+}
--- ENNB_resorted_city.R
+++ ENNB_resorted_city.R
@@ -6,7 +6,6 @@ source("TwoStage_Package_Code.R")
 path<-as.list(dir(pattern=".*cleaned.*"))
 my.data<-lapply(path, function(x) read.csv(x, header=T))
 factors<-read.csv("phyllo_factors.csv",header=T)
-rownames(factors)<-factors$treatment<-NULL
 
 for (i in 1:length(my.data)){
   my.data[[i]][,19]<-NA
@@ -17,18 +16,37 @@ for (i in 1:length(my.data)){
 #Impute the new PHYLLO30 sample into the phenotype file
 factors[18,]<-NA
 factors[,1]<-sapply(factors[,1],as.character)
-factors[18,]<-c("PHYLLO30", "HF")
-factors[,1:2]<-sapply(factors[,1:2],as.factor)
+factors[18,]<-c("PHYLLO30", "HF", "drought")
+factors[,1:3]<-sapply(factors[,1:3],as.factor)
 
-#Resort the samples according to which samples come from which city for the analysis
+rownames(factors)<-NULL
+CA.obs<-which(factors$city=="CA")
+CA.group<-factors[CA.obs,]
+HF.obs<-which(factors$city=="HF")
+HF.group<-factors[HF.obs,]
+DE.obs<-which(factors$city=="DE")
+DE.group<-factors[DE.obs,]
+rownames(CA.group)<-CA.group$city<-rownames(DE.group)<-DE.group$city<-rownames(HF.group)<-HF.group$city<-NULL
+
+CA.list<-DE.list<-HF.list<-list()
 for (i in 1:length(my.data)){
-  my.data[[i]]<-my.data[[i]][,c(1,2,15,16,17:19,3,8,12,9,13,14,4,6,10,5,7,11)]
+  CA.list[[i]]<-my.data[[i]][,c(1,CA.obs+1)]
+  DE.list[[i]]<-my.data[[i]][,c(1,DE.obs+1)]
+  HF.list[[i]]<-my.data[[i]][,c(1,HF.obs+1)]
 }
+
+#rearrange based on treatment (HF is conveniently already in order, so no need to rearrange it)
+DE.list<-lapply(DE.list, function(x) x[,c(1,2,3,5,4,6,7)])
+CA.list<-lapply(CA.list, function(x) x[,c(1,2,4,6,3,5,7)])
 
 #NOTE: These filenames are used in this order 
 #since this is their order in the original folder (sorted alphabetically)
 filenames<-list("ECID", "GOFuncID","GOProcID","PFamID","Phyllo_Genus","TIGRFamID")
 for (i in 1:length(my.data)){
-  try(TwoStage_Package(my.data[[i]], factors, paste(filenames[[i]],"citysort","sigtest","csv", sep="."), 1))
-  try(TwoStage_Package(my.data[[i]], factors, paste(filenames[[i]],"citysort","sigtest-method2","csv",sep="."), 2))
+  try(TwoStage_Package(HF.list[[i]], HF.group, paste(filenames[[i]],"analysis-HF","sigtest","csv", sep="."), 1))
+  try(TwoStage_Package(HF.list[[i]], HF.group, paste(filenames[[i]],"analysis-HF","sigtest-method2","csv",sep="."), 2))
+  try(TwoStage_Package(DE.list[[i]], DE.group, paste(filenames[[i]],"analysis-DE","sigtest","csv", sep="."), 1))
+  try(TwoStage_Package(DE.list[[i]], DE.group, paste(filenames[[i]],"analysis-DE","sigtest-method2","csv",sep="."), 2))
+  try(TwoStage_Package(CA.list[[i]], CA.group, paste(filenames[[i]],"analysis-CA","sigtest","csv", sep="."), 1))
+  try(TwoStage_Package(CA.list[[i]], CA.group, paste(filenames[[i]],"analysis-CA","sigtest-method2","csv",sep="."), 2))
 }
