ee01518d96415cfebdea40acc5f8ae8855d3109b
Talleyman <stephen522@earthlink.net>
2015-08-20 16:17:31 -0400

    Revised trimmed means

--- Trimmed_Means.R
+++ Trimmed_Means.R
@@ -1,10 +1,11 @@
+#Calculating the trimmed means, sorted by treatment and separated by city
 setwd("~/GitHub/Metagenomic-Analysis-Part-2")
-#Grab all cleaned data
 path<-as.list(dir(pattern=".*cleaned.*"))
 my.data<-lapply(path, function(x) read.csv(x, header=T))
 #Remove relevant rows, row names, and the city column so the phenotype file will run properly
-factors<-read.csv("phyllo_factors.csv",header=T)[-c(1,2, 9:12),]
-rownames(factors)<-factors$city<-NULL
+factors<-read.csv("phyllo_factors.csv",header=T)
+factors.treat<-factors[-c(1,2),]
+rownames(factors.treat)<-rownames(factors)<-factors.treat$city<-NULL
 
 #Trimmed mean function (from the edgeR paper)
 #Function borrowed from the Two_Stage Package.R file
@@ -26,19 +27,24 @@ TMMNorm = function(X, Y, TMM.option){
   }
   return(X.output)
 }
+
 #Function for removing the PHYLLO09 and PHYLLO10 entries
 removeExtras<-function(data){
   newdata<-data[,-c(2:3)]
   return(newdata)
 }
+treat.data<-lapply(my.data, removeExtras)
+for (i in 1:length(my.data)){
+  treat.data[[i]]<-treat.data[[i]][,c(1,2,4,6,8,10,13,14,3,5,7,9,11,12,15:16)]
+}
+
 #Save annotation to be bound to the data frame later
 rows.list<-lapply(my.data, function(x) return(x$X))
-my.data<-lapply(my.data, removeExtras)
-my.new.data<-lapply(my.data, function(x) return (x[,-1]))
+treat.data<-lapply(treat.data, function(x) return (x[,-1]))
 trimmed.means.norm<-new.data<-list()
 #Run the trimmed mean function with the normalization method
 for (i in 1:length(my.data)){
-  trimmed.means.norm[[i]]<-TMMNorm(my.new.data[[i]], factors, 1)
+  trimmed.means.norm[[i]]<-TMMNorm(treat.data[[i]], factors, 1)
   new.data[[i]]<-cbind(rows.list[[i]], trimmed.means.norm[[i]])
   write.csv(new.data[[i]], file=paste(unlist(strsplit(path[[i]], "-"))[1], "trimmed-means", "csv", sep="."), row.names=F)
 }
