c2500967779ac273fe98b55bfc17de72420aee48
Talleyman <stephen522@earthlink.net>
2015-08-21 20:48:20 -0400

    Added trimmed means separated by city

--- Trimmed_Means.R
+++ Trimmed_Means.R
@@ -1,4 +1,4 @@
-#Calculating the trimmed means, sorted by treatment and separated by city
+#Calculating the trimmed means, both sorted by treatment and separated by city
 setwd("~/GitHub/Metagenomic-Analysis-Part-2")
 path<-as.list(dir(pattern=".*cleaned.*"))
 my.data<-lapply(path, function(x) read.csv(x, header=T))
@@ -48,3 +48,55 @@ for (i in 1:length(my.data)){
   new.data[[i]]<-cbind(rows.list[[i]], trimmed.means.norm[[i]])
   write.csv(new.data[[i]], file=paste(unlist(strsplit(path[[i]], "-"))[1], "trimmed-means", "csv", sep="."), row.names=F)
 }
+
+#Run the trimmed means based on city, subset treatment
+#First, remove variables from previous analysis to save memory
+remove(treat.data)
+remove(factors.treat)
+remove(new.data)
+remove(trimmed.means.norm)
+
+city.data<-my.data
+remove(my.data)
+for (i in 1:length(city.data)){
+  city.data[[i]][,19]<-NA
+  colnames(city.data[[i]])[19]<-"PHYLLO30"
+  city.data[[i]]$PHYLLO30<-round((city.data[[i]]$PHYLLO28+city.data[[i]]$PHYLLO29)/2)
+}
+
+factors[18,]<-NA
+factors[,1]<-sapply(factors[,1],as.character)
+factors[18,]<-c("PHYLLO30", "HF", "drought")
+factors[,1:3]<-sapply(factors[,1:3],as.factor)
+
+CA.obs<-which(factors$city=="CA")
+CA.group<-factors[CA.obs,]
+HF.obs<-which(factors$city=="HF")
+HF.group<-factors[HF.obs,]
+DE.obs<-which(factors$city=="DE")
+DE.group<-factors[DE.obs,]
+rownames(CA.group)<-CA.group$city<-rownames(DE.group)<-DE.group$city<-rownames(HF.group)<-HF.group$city<-NULL
+
+CA.list<-DE.list<-HF.list<-list()
+for (i in 1:length(my.data)){
+  CA.list[[i]]<-city.data[[i]][,c(CA.obs+1)]
+  DE.list[[i]]<-city.data[[i]][,c(DE.obs+1)]
+  HF.list[[i]]<-city.data[[i]][,c(HF.obs+1)]
+}
+DE.list<-lapply(DE.list, function(x) x[,c(1,2,4,3,5,6)])
+CA.list<-lapply(CA.list, function(x) x[,c(1,3,5,2,4,6)])
+trimmed.means.HF<-lapply(HF.list, function(x) TMMNorm(x, HF.group, 1))
+trimmed.means.DE<-lapply(DE.list, function(x) TMMNorm(x, DE.group, 1))
+trimmed.means.CA<-lapply(CA.list, function(x) TMMNorm(x, CA.group, 1))
+complete.data.HF<-complete.data.DE<-complete.data.CA<-list()
+for (i in 1:length(trimmed.means.HF)){
+  complete.data.HF[[i]]<-cbind(rows.list[[i]], trimmed.means.HF[[i]])
+  write.csv(complete.data.HF[[i]], file=paste(unlist(strsplit(path[[i]], "-"))[1],"city-HF","trimmed-means", "csv", sep="."), row.names=F)
+  complete.data.DE[[i]]<-cbind(rows.list[[i]], trimmed.means.DE[[i]])
+  write.csv(complete.data.DE[[i]], file=paste(unlist(strsplit(path[[i]], "-"))[1],"city-DE","trimmed-means", "csv", sep="."), row.names=F)
+  complete.data.CA[[i]]<-cbind(rows.list[[i]], trimmed.means.CA[[i]])
+  write.csv(complete.data.CA[[i]], file=paste(unlist(strsplit(path[[i]], "-"))[1],"city-CA","trimmed-means", "csv", sep="."), row.names=F)
+}
+remove(trimmed.means.HF)
+remove(trimmed.means.DE)
+remove(trimmed.means.CA)
